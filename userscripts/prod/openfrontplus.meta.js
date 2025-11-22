3f0a14cfadefe3c862b6d0984428f4fc65c2df6a
Spyke <vidal.quentin@pog-group.com>
2025-11-23 00:17:44 +0100

    v0.2.0

---
+++ userscripts/prod/openfrontplus.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            OpenfrontIO+
+// @namespace       http://openfront.io/
+// @version         0.2.0
+// @description     A userscript to take Openfront to the next step for Players and Casters !
+// @author          ImTheSpyke
+// @match           *://openfront.io/*
+// @icon            https://openfront.io/images/Favicon.43987b30b9ee70769cb8.svg
+// @grant           none
+// @downloadURL     https://raw.githubusercontent.com/ImTheSpyke/openfrontplus-userscript/main/userscripts/prod/openfrontplus.user.js
+// @updateURL       https://raw.githubusercontent.com/ImTheSpyke/openfrontplus-userscript/main/userscripts/prod/openfrontplus.meta.js
+// ==/UserScript==
--- userscripts/prod/openfrontplus.user.js
+++ userscripts/prod/openfrontplus.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
-// @name         OpenfrontIO+
-// @namespace    http://openfront.io/
-// @version      0.1.3
-// @description  A userscript to take Openfront to the next step !
-// @author       ImTheSpyke
-// @match        *://openfront.io/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
+// @name            OpenfrontIO+
+// @namespace       http://openfront.io/
+// @version         0.2.0
+// @description     A userscript to take Openfront to the next step for Players and Casters !
+// @author          ImTheSpyke
+// @match           *://openfront.io/*
+// @icon            https://openfront.io/images/Favicon.43987b30b9ee70769cb8.svg
+// @grant           none
+// @downloadURL     https://raw.githubusercontent.com/ImTheSpyke/openfrontplus-userscript/main/userscripts/prod/openfrontplus.user.js
+// @updateURL       https://raw.githubusercontent.com/ImTheSpyke/openfrontplus-userscript/main/userscripts/prod/openfrontplus.meta.js
 // ==/UserScript==
 
 
