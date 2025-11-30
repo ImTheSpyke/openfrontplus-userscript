037fab22dbcbfbf28663af74f3f2ebada4bc0ca5
Spyke <vidal.quentin@pog-group.com>
2025-12-01 00:18:16 +0100

    0.3.0 - Added hover menu on bottom right of mouse

--- userscripts/prod/openfrontplus.meta.js
+++ userscripts/prod/openfrontplus.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            OpenfrontIO+
 // @namespace       http://openfront.io/
-// @version         0.2.1
+// @version         0.3.0
 // @description     A userscript to take Openfront to the next step for Players and Casters !
 // @author          ImTheSpyke
 // @match           *://openfront.io/*
